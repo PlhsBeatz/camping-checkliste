b0f968d23824b14540f80a868bd257e5ecde35d1
PlhsBeatz <plhsbeatz@gmail.com>
2026-05-10 14:07:47 +0200

    refactor: update form field styling to use bg-field for improved design consistency across input components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,7 @@ export default {
   			},
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
+  			field: 'hsl(var(--field))',
   			ring: 'hsl(var(--ring))',
   			chart: {
   				'1': 'hsl(var(--chart-1))',
